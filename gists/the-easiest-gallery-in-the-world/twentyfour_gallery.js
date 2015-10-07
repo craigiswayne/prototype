89bd646842e1dfd97cce51868a8afcbbef3987fc
Craig Wayne <craig.wayne01@gmail.com>
2015-10-07 16:04:35 +0200

    updates to the twentyfour gallery

--- gists/the-easiest-gallery-in-the-world/twentyfour_gallery.js
+++ gists/the-easiest-gallery-in-the-world/twentyfour_gallery.js
@@ -19,87 +19,10 @@
   }(jQuery));
 //end extend jquery
 
-$(document).ready(function(){
-    set_active_links();
-});
-
-function set_active_links(){
-    $("a[href]").each(function(){
-        if(document.location.href == this.href){
-            $(this).addClass("active");
-            //TODO THIS MUST GET DONE ON THE SERVER SIDE IF POSSIBLE
-        }
-    });
-}
-
 var nexus = {
-  init: function(){
-    //todo find all init functions in the nexus object and run them
-    nexus.init_mask();
-    nexus.init_tables();
-    //todo monitor changes and auto run for input type time
-    nexus.init_timepickers();
-  },
-  init_mask: function(){
-    nexus.mask = $('.mask'),
-    nexus.mask.show = function(){
-      $('.mask').show();
-      $('body').addClass('blur');
-    };
-    nexus.mask.hide = function(){
-      $('.mask').hide();
-      $('body').removeClass('blur');
-    };
-  },
-  init_tables: function(){
-
-    $('table.sortable').each(function(i){
-        $(this).find('thead th .fa-sort').each(function(j){
-            $(this).click(function(){
-              //todo sort here
-            })
-        });
-    });
-
-    $('table').each(function(i){
-      this.add_row = function(){
-        var template = nexus.get_template(this.id);
-        if(template){
-          //window.cw = template;
-          //console.debug($(template));
-          //console.debug($(template).find('tr')[0]);
-          var new_row = this.tBodies[0].appendChild(document.createElement("tr"));
-          $(new_row).replaceWith(template);
-
-        }
-      };
-    });
-  },
-  init_timepickers: function(){
-    $('input[type=time].nexus').each(function(i){
-        $(this).attr('type','text');
-        $(this).timepicker();
-    });
-
-  },
   parse_template: function(template,data){
     template = template || '';
     data = data || {};
-    /*todod
-    var check = mixed_input.split(" ");
-    var template = null;
-    if(check.length > 1){
-      template = mixed_input;
-    }
-    else{
-      template = nexus.find_template(mixed_input) || '';
-    }*/
-    //
-    // var re = /\(#(.*?)#\)/g;
-    // var str = 'hello (#world#) (#city#)';
-    // var subst = 'test';
-    //
-    // var result = str.replace(re, subst);
 
     var re = /\(#(.*?)#\)/g;
     var str = template;
@@ -138,12 +61,6 @@ var nexus = {
   }
 };
 
-$(document).ready(nexus.init);
-
-//window.history.pushState([], "Title", "/new-url");
-
-
-
 
 function twentyfour_gallery (selector, settings){
   this.prefix = "twentyfour";
@@ -171,7 +88,7 @@ function twentyfour_gallery (selector, settings){
 
   this.settings = {
     container_class:"twentyfour gallery",
-    fullscreen_icon_class:"icon-screen-full",
+    fullscreen_icon_class:"icon-expand",
     play_icon_class: "icon-playback-play",
     pause_icon_class:"icon-playback-pause",
     nav_left_icon_class:"icon-angle-left",
@@ -184,27 +101,20 @@ function twentyfour_gallery (selector, settings){
     query_selector: ".gallery24",
     show_speed:3000,
     autolink_css:true,
-    thumbnail_selector:'.thumbnail'
+    thumbnail_selector:'.thumbnail',
+    minimum_slides:2
   };
 
   this.go_fullscreen = function(){
     var container = this.container[0];
     if (container.requestFullscreen) {
       container.requestFullscreen();
-      //siteGallery.galleryComponent.addClass('fullscreen');
-      //siteGallery.goToSlide(siteGallery.slideIndex);
     } else if (container.mozRequestFullScreen) {
       container.mozRequestFullScreen();
-      //siteGallery.galleryComponent.addClass('fullscreen');
-      //siteGallery.goToSlide(siteGallery.slideIndex);
     } else if (container.webkitRequestFullscreen) {
       container.webkitRequestFullscreen();
-      //siteGallery.galleryComponent.addClass('fullscreen');
-      //siteGallery.goToSlide(siteGallery.slideIndex);
     } else if (container.msRequestFullscreen) {
       container.msRequestFullscreen();
-      //siteGallery.galleryComponent.addClass('fullscreen');
-      //siteGallery.goToSlide(siteGallery.slideIndex);
     }
     this.container.addClass("fullscreen");
   };
@@ -396,259 +306,45 @@ function twentyfour_gallery (selector, settings){
   };
 
   this.init = function(selector,settings){
+      gallery = this;
       this.selector = selector || this.settings.query_selector;
       this.container = $(selector).first();
-      this.attributes = this.container.attr();
-      $.extend(this.settings, settings,this.container.data());
-      gallery = this;
+      //todo allow for multiple galleries with one selector
 
       var slides = $(this.container).find(".slide");
+      if(slides.length < this.settings.minimum_slides){
+        console.group("TwentyFour Gallery");
+        console.info("too few slides");
+        console.groupEnd();
+        
+        return null;
+      }
       $(slides).each(function(index){
         gallery.slides[index] = $(this);
       });
 
+      this.attributes = this.container.attr();
+      $.extend(this.settings, settings,this.container.data());
+
+
+
       var generated_id = this.container.attr("id") || this.prefix + "_gallery_" + Math.round(Math.random()*100);
       this.update_templates({"id":generated_id});
       this.container.replaceWith(this.templates.gallery);
+
       this.container  = $("#" + generated_id);
+      //todo you gotta check that there are no duplicated id's
+
       this.stage      = $(this.container).find(">.stage");
       this.tray       = $(this.container).find(">.tray");
       this.media_bar  = $(this.container).find(">.media_bar");
-      //todo sort out the buttons initialize function
       this.buttons.fullscreen = this.container.find(".icon.fullscreen");
-      //console.debug($(this.element).outerHTML);
-      //this.element = $(this.element).replaceWith(this.templates.gallery);
       this.add_event_listeners();
-      //todo allow for multiple results from selctor
       this.go_to_slide(0);
       this.container.addClass("ready");
-      console.debug(this.settings);
+
       return this;
   }.bind(this);
 
   return this.init(selector,settings);
 }
-
-
-// var siteGallery = {
-//     galleryComponent: null,
-//     slideIndex: null,
-//     totalSlides: 0,
-//     canFullscreen: false,
-//     goFullScreen: function () {
-//
-//         var docElement, request;
-//         docElement = document.getElementById('galleryComponent');
-//
-//         element = docElement;
-//         if (element.requestFullscreen) {
-//
-//             element.requestFullscreen();
-//             siteGallery.galleryComponent.addClass('fullscreen');
-//             siteGallery.goToSlide(siteGallery.slideIndex);
-//         } else if (element.mozRequestFullScreen) {
-//             element.mozRequestFullScreen();
-//             siteGallery.galleryComponent.addClass('fullscreen');
-//             siteGallery.goToSlide(siteGallery.slideIndex);
-//         } else if (element.webkitRequestFullscreen) {
-//
-//             element.webkitRequestFullscreen();
-//             siteGallery.galleryComponent.addClass('fullscreen');
-//             siteGallery.goToSlide(siteGallery.slideIndex);
-//         } else if (element.msRequestFullscreen) {
-//             element.msRequestFullscreen();
-//             siteGallery.galleryComponent.addClass('fullscreen');
-//             siteGallery.goToSlide(siteGallery.slideIndex);
-//         }
-//     },
-//     exitFullScreen: function () {
-//
-//         if (document.exitFullscreen) {
-//             document.exitFullscreen();
-//         } else if (document.mozCancelFullScreen) {
-//             document.mozCancelFullScreen();
-//         } else if (document.webkitExitFullscreen) {
-//             document.webkitExitFullscreen();
-//         } else if (document.msExitFullscreen) {
-//             document.msExitFullscreen();
-//         }
-//
-//         siteGallery.galleryComponent.removeClass('fullscreen');
-//         siteGallery.goToSlide(siteGallery.slideIndex);
-//     },
-//     checkFullscreenFunctionality: function () {
-//         var element = document.getElementById('galleryComponent');
-//         if (element.requestFullscreen) {
-//             siteGallery.canFullscreen = true;
-//         } else if (element.mozRequestFullScreen) {
-//             siteGallery.canFullscreen = true;
-//         } else if (element.webkitRequestFullscreen) {
-//             siteGallery.canFullscreen = true;
-//         } else if (element.msRequestFullscreen) {
-//             siteGallery.canFullscreen = true;
-//         }
-//
-//         if (siteGallery.canFullscreen != true) {
-//             siteGallery.galleryComponent.addClass('no_fullscreen');
-//         }
-//     },
-//     init: function () {
-//         this.galleryComponent = this.galleryComponent || $j("#galleryComponent");
-//         siteGallery.checkFullscreenFunctionality();
-//         //icons
-//         this.galleryComponent.find(".stage").append("<a class='icon icon-expand fullscreen'></a>");
-//         this.galleryComponent.find(".stage").append("<a class='icon icon-compress minimize'></a>");
-//         this.galleryComponent.find(".stage").append("<div class='nav_container left'><a class='icon icon-arrow-circle-o-left nav left'></a></div>");
-//         this.galleryComponent.find(".stage").append("<div class='nav_container right'><a class='icon icon-arrow-circle-o-right nav right'></a></div>");
-//         this.galleryComponent.find(".thumbnails.widget").append("<a class='icon icon-angle-left nav left'></a>");
-//         this.galleryComponent.find(".thumbnails.widget").append("<a class='icon icon-angle-right nav right'></a>");
-//
-//         //event listeners
-//         //fullscreen
-//         this.galleryComponent.find('.stage .icon.fullscreen').on('click', function () {
-//             siteGallery.goFullScreen();
-//         });
-//
-//         this.galleryComponent.find('.stage .icon.minimize').on('click', function () {
-//             siteGallery.exitFullScreen();
-//         });
-//
-//         $j(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function (e) {
-//
-//             var state = document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenEnabled || document.fullScreen;
-//             var event = state ? 'FullscreenOn' : 'FullscreenOff';
-//             if (document.msFullscreenEnabled && document.msFullscreenElement == null) {
-//                 event = 'FullscreenOff';
-//             }
-//             if (event == 'FullscreenOff') {
-//                 siteGallery.exitFullScreen();
-//             }
-//         });
-//
-//         this.galleryComponent.find('.stage .icon.nav.left').click(function () {
-//             siteGallery.goToSlide(siteGallery.slideIndex - 1);
-//         });
-//
-//         this.galleryComponent.find('.stage .icon.nav.right').click(function () {
-//             siteGallery.goToSlide(siteGallery.slideIndex + 1);
-//         });
-//
-//         this.galleryComponent.find('.thumbnails.widget .icon.nav.left').click(function () {
-//             siteGallery.goToSlide(siteGallery.slideIndex - 1);
-//         });
-//
-//         this.galleryComponent.find('.thumbnails.widget .icon.nav.right').click(function () {
-//             siteGallery.goToSlide(siteGallery.slideIndex + 1);
-//         });
-//
-//         this.galleryComponent.find(".widget.thumbnails .item").each(function (i) {
-//             siteGallery.totalSlides++;
-//             $j(this).click(function () {
-//                 siteGallery.goToSlide(i);
-//                 return false;
-//             });
-//         });
-//
-//         if (siteGallery.galleryComponent.hasClass('images')) {
-//             //center large images
-//             $j(this.galleryComponent).find('.stage .slider .slide img').each(function (i) {
-//                 $j(this).parent().css('background-image', 'url(' + $j(this).attr('src') + ')');
-//                 $j(this).remove();
-//             });
-//         }
-//
-//
-//         if (siteGallery.galleryComponent.hasClass('video')) {
-//             siteGallery.initVideos();
-//         }
-//
-//         //fix for thumbnail widget
-//         if(siteGallery.totalSlides)
-//
-//         this.goToSlide(0);
-//     },
-//     initVideos: function () {
-//
-//         var tag = document.createElement('script');
-//
-//         tag.src = "https://www.youtube.com/iframe_api";
-//         var firstScriptTag = document.getElementsByTagName('script')[0];
-//         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-//
-//         siteGallery.galleryComponent.find('.stage .slider .item .media').each(function (i) {
-//             var video_url = $j(this).attr("src");
-//
-//             if (video_url.indexOf("youtube") > -1 || video_url.indexOf("youtu.be") > -1) {
-//                 $j(this).replaceWith('<div class="media youtube" src="' + $j(this).attr("src") + '"></div');
-//             }
-//             else if (video_url.indexOf("cdn.24.co.za") > -1) {
-//                 var new_tag_id = "jwplayer_"+i;
-//                 $j(this).replaceWith('<div class="media jwplayer" id="' + new_tag_id + '"></div');
-//
-//                 var playerInstance = jwplayer(new_tag_id);
-//                 playerInstance.setup({
-//                     file: video_url,
-//                     image: "http://dev.cdn.24.co.za/files/Cms/General/d/682/ae0b812114644fe59ec692ef3e9df42a.jpg",
-//                     width: siteGallery.galleryComponent.width(),
-//                     height: siteGallery.galleryComponent.find(".stage").height(),
-//                     title: $j(this).attr("title"),
-//                     description: $j(this).parent().find(".caption").text()
-//                 });
-//             }
-//         });
-//
-//         window.onYouTubeIframeAPIReady = function() {
-//             siteGallery.galleryComponent.find('.stage .slider .slide .youtube').each(function () {
-//                 var source = $j(this).attr('src');
-//                 var videoId = null;
-//                 if (source.indexOf("youtu.be") > -1) {
-//                     videoId = source.substring(source.indexOf("youtu.be") + 9, source.length);
-//                 } else {
-//                     videoId = source.substring(source.indexOf('watch?v=') + 8, source.length);
-//                 }
-//
-//                 var player = new YT.Player(this, {
-//                     height: 'auto',
-//                     width: siteGallery.galleryComponent.width(),
-//                     videoId: videoId
-//                 });
-//             });
-//         };
-//     },
-//     goToSlide: function (index) {
-//         //zeroBased
-//         index = (index < 0) ? 0 : index;
-//         index = (index >= siteGallery.totalSlides) ? siteGallery.totalSlides - 1 : index;
-//         siteGallery.slideIndex = index;
-//         var slideWidth = this.galleryComponent.find('.stage .slider').width();
-//         siteGallery.galleryComponent.find('.stage .slider').css('left', -(index * slideWidth));
-//
-//         siteGallery.galleryComponent.find('.widget.thumbnails .item').removeClass('active');
-//         $j(siteGallery.galleryComponent.find('.widget.thumbnails .item')[index]).addClass('active');
-//
-//         siteGallery.galleryComponent.removeClass('first');
-//         siteGallery.galleryComponent.removeClass('last');
-//
-//         if (index == 0) {
-//             siteGallery.galleryComponent.addClass('first');
-//         }
-//
-//         if (index == siteGallery.totalSlides - 1) {
-//             siteGallery.galleryComponent.addClass('last');
-//         }
-//
-//         if ($j(siteGallery.galleryComponent).not('.fullscreen')) {
-//
-//             if (index > 5) {
-//                 var shift_multiplier = index - 5;
-//                 siteGallery.galleryComponent.find('.thumbnails.widget .slider').css('left', "-" + (shift_multiplier * 90) + 'px');
-//             }
-//             else {
-//                 siteGallery.galleryComponent.find('.thumbnails.widget .slider').css('left', "0");
-//             }
-//         }
-//     }
-// };
-// $j(document).ready(function () {
-//     siteGallery.init();
-// });
