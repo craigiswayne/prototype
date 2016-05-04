71c06c2377618b89fcb066efbe2b813188812acb
Craig Wayne <craigiswayne@gmail.com>
2016-05-04 12:25:25 +0200

    nothing really

--- nexus.js
+++ nexus.js
@@ -11,7 +11,7 @@ String.prototype.parse_color = function(){
      n = Math.max(0,Math.min(n,255));
      return "0123456789ABCDEF".charAt((n-n%16)/16)
           + "0123456789ABCDEF".charAt(n%16);
-    }    
+    }
 
     if(color.indexOf("rgb(") === 0 && color.length <= 16 && color.length >= 10 && color.indexOf(")") == color.length -1){
         color = color.replace("rgb(","").replace(")","").split(",");
@@ -40,7 +40,7 @@ String.prototype.extension = function(){
     var string = this.trim();
     var extension = null;
     if(string.lastIndexOf(".") > -1){
-        if(string.length > string.lastIndexOf(".") + 1){   
+        if(string.length > string.lastIndexOf(".") + 1){
             extension = string.substring(string.lastIndexOf(".")+1,string.length);
         }
     }
@@ -48,15 +48,15 @@ String.prototype.extension = function(){
 };
 
 var nexus = function(){
-	
+
     nexus.notify = function(){
-        
+
     };
-    
+
     //nexus error module
     nexus.error = function(title,message){
          if(window.location.search.indexOf("debug=true") < 0) return;
-        
+
 		if(message) console.error(title,message);
 		else console.error(title);
     };
@@ -66,7 +66,7 @@ var nexus = function(){
 	//nexus google maps module
 	nexus.google = {};
 	nexus.google.maps = {
-		
+
 		api_url     : "https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&callback=nexus.google.maps.post_install",
 		installed   : false,
 		options     : {
@@ -74,12 +74,12 @@ var nexus = function(){
             latitude: -34.397,
             longitude: 150.644
 		},
-		
+
 		post_install: function(){
 			nexus.debug("Google Map API installtion complete...");
 			nexus.google.maps.installed = true;
 		},
-		
+
 		create      : function(element){
 			if(!nexus.google.maps.installed){
 				nexus.google.maps.install(function(){
@@ -105,30 +105,30 @@ var nexus = function(){
 			}
 
 		},
-		
+
 		install     : function(callback){
 			nexus.debug("Installing Google Map API...", nexus.google.maps.api_url);
 			nexus.scripts.install(nexus.google.maps.api_url);
 		},
-		
+
 		init        : function(){
 			//check if the google map script is installed
 			for(var i=0; i<document.scripts.length; i++){
 				if(document.scripts[i].src){
-					if(document.scripts[i].src == nexus.google.maps.api_url) return; 
+					if(document.scripts[i].src == nexus.google.maps.api_url) return;
 				}
 			}
 			nexus.google.maps.install();
 		}
 	};
-	
+
     //nexus mask module
     nexus.hide_mask = function(force){
-        
+
         force = (typeof force == "boolean") ? force : false;
 		var source;
         if(event){source = event.srcElement || event.target;}
-        
+
 		if(force || source == document.querySelector(".mask>table.aligner") || source == document.querySelector(".mask>table.aligner td.aligner")){
             $(document.querySelector(".mask")).removeClass("active");
 			document.querySelector(".mask").innerHTML = "";
@@ -136,7 +136,7 @@ var nexus = function(){
     };
     nexus.show_mask   = function(content){
         content = content || "";
-        
+
         var mask;
         if(!document.querySelector(".mask")){
             mask = document.body.appendChild(document.createElement("div"));
@@ -145,32 +145,32 @@ var nexus = function(){
         else{
             mask = document.querySelector(".mask");
         }
-        
+
         mask.innerHTML  = "<table class='aligner'><tr class='aligner'><td class='aligner'><div class='content popup animate'></div></td></tr></table>";
-        
+
         if(content.trim() != ""){content_div.innerHTML   = content;}
-        
+
         $(mask).addClass("mask");
         $(mask).addClass("active");
     };
-     
+
 	//nexus file module
 	nexus.file = {
         read_san:function(files){
-            
+
             nexus.debug(this);
             nexus.debug(event);
-            
+
             if(!event || !event.srcElement || !event.srcElement instanceof HTMLInputElement || !event.srcElement.type == "file"){return;}
-            
+
             var file_input = event.srcElement;
             files = files || file_input.files;
-            
-            if(files.length == 0){nexus.error("No files present"); return;} 
-            
+
+            if(files.length == 0){nexus.error("No files present"); return;}
+
             var f = files[0];
             var reader = new FileReader();
-            
+
             reader.onload = function(e) {
                 var data = e.target.result;
                 var worker = new Worker('./xlsxworker2.js');
@@ -179,14 +179,14 @@ var nexus = function(){
                         case 'ready': break;
 
                         case 'e': console.error(e.data.d); break;
-                        default: 
+                        default:
 
                             var o = "", l = 0, w = 10240;
                             for(; l<e.data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint16Array(e.data.slice(l*w,l*w+w)));
                             o+=String.fromCharCode.apply(null, new Uint16Array(e.data.slice(l*w)));
 
-                            xx=o.replace(/\n/g,"\\n").replace(/\r/g,"\\r"); 
-                            
+                            xx=o.replace(/\n/g,"\\n").replace(/\r/g,"\\r");
+
                             var temp_2 = JSON.parse(xx);
                             var result = {};
                             temp_2.SheetNames.forEach(function(sheetName) {
@@ -211,7 +211,7 @@ var nexus = function(){
             };
             reader.readAsBinaryString(f);
         },
-        
+
         image_to_base64: function(input){
             /*//function convertImgToBase64URL(url, callback, outputFormat){
             //credit: http://stackoverflow.com/questions/6150289/how-to-convert-image-into-base64-string-using-javascript
@@ -225,14 +225,14 @@ var nexus = function(){
                 ctx.drawImage(img, 0, 0);
                 dataURL = canvas.toDataURL(outputFormat);
                 return dataURL;
-                canvas = null; 
+                canvas = null;
             };
             img.src = url;
             //}*/
-            
+
             //credit: http://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
             //credit: http://jsfiddle.net/LvsYc/
-            
+
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = function(e){
@@ -252,10 +252,10 @@ var nexus = function(){
             };
             var blob = file.slice(0, file.size);
             reader.readAsBinaryString(blob);
-            
+
             return dfd.promise();
         }
-        
+
     },
     nexus.dropzone = function(element,settings){
         //ref http://html5demos.com/drag#view-source
@@ -265,7 +265,7 @@ var nexus = function(){
         settings.ondragenter    = settings.ondragenter      || function(){};
         settings.ondragover     = settings.ondragover       || function(){};
         settings.ondragleave    = settings.ondragleave      || function(){};
-        
+
         if(!element) return null;
 
         $(element).addClass("dropzone");
@@ -296,7 +296,7 @@ var nexus = function(){
             e.preventDefault();
             this.remove_drag_states();
             $(this).addClass("drag drop");
-            
+
             var files = e.dataTransfer.files;
             for (var i = 0, f; f = files[i]; i++) {
                 var file = null;
@@ -307,9 +307,9 @@ var nexus = function(){
                 settings.ondrop(file);
             }
             this.remove_drag_states();
-            
+
         };
-        
+
         element.remove_drag_states = function(){
             $(this).removeClass("drag");
             $(this).removeClass("over");
@@ -321,7 +321,7 @@ var nexus = function(){
         return element;
 
     };
-    
+
 	//nexus json module
     nexus.json = {
 		table:function(json, options){
@@ -329,9 +329,9 @@ var nexus = function(){
             options = options || {};
             //options.generate_header = options.generate_header || true;
             nexus.debug(options);
-            
+
             json = json || {};
-            
+
             var table = document.createElement("table");
             table.className                 = "nexus enable_row_select";
             var table_header                = table.appendChild(document.createElement("thead"));
@@ -341,7 +341,7 @@ var nexus = function(){
             table_header_checkbox.type      = "checkbox";
             table_header_checkbox.className = "nexus";
             table_header_checkbox.addEventListener("change",function(){
-            
+
                 var table = this.parentNode.parentNode.parentNode.parentNode;
                 var row_checkboxes = table.querySelectorAll(".row_checkbox_cell input[type=checkbox].row_checkbox");
                 for(var i=0; i<row_checkboxes.length; i++){
@@ -350,17 +350,17 @@ var nexus = function(){
     				$(row_checkboxes[i]).trigger("change");
 					//$(row_checkboxes[i]).prop('checked', true).change();
                 }
-                
+
             },false);
-            
+
             if(options.generate_header === true){
-                
+
                 for(var i in Object.keys(json)){
                     var header_cell = table_header_row.appendChild(document.createElement("th"));
                     header_cell.innerHTML = Object.keys(json)[i];
                 }
             }
-            
+
             var table_body = table.appendChild(document.createElement("tbody"));
             for(var i in Object.keys(json)){
                 var row_entry               = table_body.appendChild(document.createElement("tr"));
@@ -376,8 +376,8 @@ var nexus = function(){
                     if(this.checked){$(this.parentNode.parentNode).addClass("selected");}
                     else{$(this.parentNode.parentNode).removeClass("selected");}
                 },false);
-                
-                
+
+
                 for(var j in Object.keys(Object.keys(json)[i])){
                     var cell_value = json[i][Object.keys(json[i])[j]];
                     if(cell_value !== undefined){
@@ -387,25 +387,25 @@ var nexus = function(){
                     }
                 }
             }
-            
+
             document.querySelector("#table_output").innerHTML = "";
             document.querySelector("#table_output").appendChild(table);
             return table;
         }
     },
-	
+
 	//nexus jquery module
 	nexus.jquery_url = "http://code.jquery.com/jquery.js";
-	
+
 	nexus.debug = function(title,message){
         if(window.location.search.indexOf("debug=true") < 0) return;
-        
+
 		if(message) console.debug(title,message);
 		else console.debug(title);
     };
-	
+
 	nexus.link = function(src){
-		
+
 		if(src.substring(src.length-4, src.length) == ".css"){
 			var link = document.createElement("link");
 			link.rel = "stylesheet";
@@ -413,14 +413,14 @@ var nexus = function(){
 			document.head.appendChild(link);
 		}
 	};
-    
+
     nexus.menu = function(json_tree){
-        
+
         var menu = document.createElement("ul");
         menu.className = "nexus menu";
-        
+
         console.debug(Object.keys(json_tree));
-        
+
         for(var i in Object.keys(json_tree)){
             var key = Object.keys(json_tree)[i];
             var menu_item = menu.appendChild(document.createElement("li"));
@@ -430,17 +430,17 @@ var nexus = function(){
                 menu_item.appendChild(new nexus.menu(json_tree[key]));
             }
         }
-        
+
         return menu;
-    
+
     };
-	
+
     nexus.show_popup =  function(params){
         params = params || {};
-        
+
         if(params.url){
             $.ajax({
-                url: params.url       
+                url: params.url
             }).done(function(result) {
                 $(".mask td.aligner .content").html(result);
             }).error(function(e,f){
@@ -452,11 +452,11 @@ var nexus = function(){
                 }
                 catch(e){
                     console.debug(e);
-                }  
+                }
             });
         }
     };
-    
+
 	//nexus scripts module
 	nexus.scripts = {};
 	nexus.scripts.install = function(src){
@@ -467,7 +467,7 @@ var nexus = function(){
 		document.body.appendChild(script);
 		return script;
 	};
-	
+
 	nexus.init = function(){
 		nexus.debug("Initializing nexus...");
 		//install jquery library
