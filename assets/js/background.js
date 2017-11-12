96145a1903d3ce13f31af3ef6dcd9cc66b3a6846
Craig Wayne <craigiswayne@gmail.com>
2017-11-12 15:46:39 +0200

    Moved assets into the assets folder

--- assets/js/background.js
+++ assets/js/background.js
@@ -4,7 +4,7 @@ chrome.app.runtime.onLaunched.addListener(function(){
 	//link chrome app styles specifically
 	var chrome_app_styles_link = document.createElement("link");
 	chrome_app_styles_link.rel = "stylesheet";
-	chrome_app_styles_link.href= "nexus_prototype_chrome_app.css";
+	chrome_app_styles_link.href= "assets/css/nexus_prototype_chrome_app.css";
 	  
 	  console.debug(app_window.contentWindow);
 	  console.debug(app_window.contentWindow.document);
