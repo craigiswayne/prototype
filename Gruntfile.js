4a45f743c065202229c8d89c3248badef5eab1c8
Craig Wayne <craigiswayne@gmail.com>
2017-11-13 22:47:54 +0200

    PWA optimizations
    Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,122 +1,138 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>'
-      }
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    watch: {
-      js:{
-          cwd: 'assets/js/',
-          files: ['*.js', '!*.min.js'],
-          tasks: ['concat', 'uglify'],
-      },
-      css: {
-          files: 'assets/css/*.less',
-          tasks: ['less'],
-          options: {
-              livereload: true
-          }
-      }
-    },
-      autoprefixer: {
-          development: {
-              browsers: ['last 2 version', 'ie 9'],
-              expand: true,
-              flatten: true,
-              src: 'assets/css/*.css',
-              dest: 'assets/css'
-          }
-      },
-      less: {
-        development: {
-          options: {
-            paths: ['assets/css']
-          },
-          files: {
-            'assets/css/style.css': 'assets/css/style.less'
-          },
-          plugins: [
-              new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions", 'ie9']})
-          ]
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            js: {
+                src: ['assets/js/nexus.js', 'assets/js/nexus_prototype.js'],
+                dest: 'assets/js/script.js'
+            }
         },
-        production: {
-          options: {
-            paths: ['assets/css']
-          },
-          files: {
-            'assets/css/style.css': 'assets/css/style.less'
-          }
-        }
-      },
+        php2html: {
+            default: {
+                options: {
+                    processLinks: false
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.',
+                        src: ['index.php'],
+                        dest: '.',
+                        ext: '.html'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            }
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                unused: true,
+                boss: true,
+                eqnull: true
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            lib_test: {
+                src: ['lib/**/*.js', 'test/**/*.js']
+            }
+        },
+        watch: {
+            js: {
+                cwd: 'assets/js/',
+                files: ['*.js', '!*.min.js'],
+                tasks: ['concat:js', 'uglify'],
+            },
+            css: {
+                files: 'assets/css/*.less',
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        autoprefixer: {
+            development: {
+                browsers: ['last 2 version', 'ie 9'],
+                expand: true,
+                flatten: true,
+                src: 'assets/css/*.css',
+                dest: 'assets/css'
+            }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ['assets/css']
+                },
+                files: {
+                    'assets/css/style.css': 'assets/css/style.less'
+                },
+                plugins: [
+                    new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions", 'ie9']})
+                ]
+            },
+            production: {
+                options: {
+                    paths: ['assets/css']
+                },
+                files: {
+                    'assets/css/style.css': 'assets/css/style.less'
+                }
+            }
+        },
 
-      
+
         cssmin: {
-          target: {
-            files: [{
-              expand: true,
-              cwd: 'assets/css/',
-              src: ['*.css', '!*.min.css'],
-              dest: 'assets/css',
-              ext: '.min.css'
-            }]
-          }
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'assets/css',
+                    ext: '.min.css'
+                }]
+            }
         }
-      
-    
-  });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  // grunt.loadNpmTasks('less-plugin-clean-css');
-  // grunt.loadNpmTasks('less-plugin-autoprefix');
-    
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-    
-  
 
-  grunt.registerTask( 'default', [ 'less', 'cssmin', 'concat', 'uglify' ] );
-  grunt.registerTask( 'prod', [ 'concat', 'uglify' ] );
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-php2html');
+
+
+    grunt.registerTask('default', ['less', 'cssmin', 'concat', 'uglify']);
+    grunt.registerTask('prod', ['concat', 'uglify']);
 
 };
--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,154 +1,155 @@
-document.addEventListener("DOMContentLoaded",function(){
-$(function() {
+document.addEventListener("DOMContentLoaded", function () {
+    $(function () {
 
-	var _win		= $(window),
-	_canvas 	= $('#canvas'),
-	_ctx 		= _canvas[0].getContext('2d'),
+        var _win = $(window),
+            _canvas = $('#canvas'),
+            _ctx = _canvas[0].getContext('2d'),
 
-	_color		= '#000',
-	_fill		= '#000',
-	_stroke 	= 1,
+            _color = '#000',
+            _fill = '#000',
+            _stroke = 1,
 
-	_cWidth 	= +_canvas.attr('width'),
-	_cHeight	= +_canvas.attr('Height'),
-	_cPos 		= {
-		x: _canvas.offset().left,
-		y: _canvas.offset().top
-	},
+            _cWidth = +_canvas.attr('width'),
+            _cHeight = +_canvas.attr('Height'),
+            _cPos = {
+                x: _canvas.offset().left,
+                y: _canvas.offset().top
+            },
 
-	_mPos = { x:0, y:0 },
-	_mOldPos = { x:0, y:0 },
-	_mStartPos = { x:0, y:0 },
-	isDrag		= false;
+            _mPos = {x: 0, y: 0},
+            _mOldPos = {x: 0, y: 0},
+            _mStartPos = {x: 0, y: 0},
+            isDrag = false;
 
-	// setup the rulers with counters and markers
-	var hRuler 	= $('#horizontalRuler'),
-	vRuler 	= $('#verticalRuler'),
-	status 	= $('#statusBar'),
-	hMarker	= $('#hMarker'),
-	vMarker	= $('#vMarker');
+        // setup the rulers with counters and markers
+        var hRuler = $('#horizontalRuler'),
+            vRuler = $('#verticalRuler'),
+            status = $('#statusBar'),
+            hMarker = $('#hMarker'),
+            vMarker = $('#vMarker');
 
-	function setupRulers() {
-		var hCount = (_cWidth + 1) / 100,
-		vCount = (_cHeight + 1) / 100,
-		hList = $('<ul/>'),
-		vList = $('<ul/>'),
-		i = 0, k;
+        function setupRulers() {
+            var hCount = (_cWidth + 1) / 100,
+                vCount = (_cHeight + 1) / 100,
+                hList = $('<ul/>'),
+                vList = $('<ul/>'),
+                i = 0, k;
 
-		// clear out the counter first
-		hRuler.find('ul').remove();
-		vRuler.find('ul').remove();
+            // clear out the counter first
+            hRuler.find('ul').remove();
+            vRuler.find('ul').remove();
 
-		// add the coordinates list
-		for ( ; i < hCount; i++) {
-			k = i * 100;
-			hList.append( $('<li>' + k + '</li>').css('left', k) );
-		}
-		hRuler.css('width', _cWidth).append(hList);
+            // add the coordinates list
+            for (; i < hCount; i++) {
+                k = i * 100;
+                hList.append($('<li>' + k + '</li>').css('left', k));
+            }
+            hRuler.css('width', _cWidth).append(hList);
 
-		for (i = 0; i < vCount; i++) {
-			k = i * 100;
-			vList.append( $('<li>' + k + '</li>').css('top', k) );
-		}
-		vRuler.css('height', _cHeight).append(vList);
-	}
-	setupRulers();
+            for (i = 0; i < vCount; i++) {
+                k = i * 100;
+                vList.append($('<li>' + k + '</li>').css('top', k));
+            }
+            vRuler.css('height', _cHeight).append(vList);
+        }
 
-	// the main move event
-	_canvas.on('mousemove', function(e) {
-		var delta, dx, dy;
+        setupRulers();
 
-		_mPos.x = e.pageX - _cPos.x + 20;
-		_mPos.y = e.pageY - _cPos.y + 20;
+        // the main move event
+        _canvas.on('mousemove', function (e) {
+            var delta, dx, dy;
 
-		// move the markers
-		hMarker.css('left', _mPos.x);
-		vMarker.css('top', _mPos.y);
+            _mPos.x = e.pageX - _cPos.x + 20;
+            _mPos.y = e.pageY - _cPos.y + 20;
 
-		// update status
-		status.empty().append(
-		'<label title="cursor position">X ' + (_mPos.x + 1 - 20)
-			+ ' Y ' + (_mPos.y + 1 - 20) + '</label>'
-			);
+            // move the markers
+            hMarker.css('left', _mPos.x);
+            vMarker.css('top', _mPos.y);
 
-			if (isDrag) {
-				delta = dist(_mPos, _mStartPos).delta;
-				status.append(
-				'<label>X<sub>1</sub> ' + _mStartPos.x 
-					+ ' Y<sub>1</sub> ' + _mStartPos.y + '</label>'
-					+ '<label>&Delta; ' + delta + '</label>'
-					);
-				}
+            // update status
+            status.empty().append(
+                '<label title="cursor position">X ' + (_mPos.x + 1 - 20)
+                + ' Y ' + (_mPos.y + 1 - 20) + '</label>'
+            );
 
-				// record the position for next frame
-				_mOldPos = _mPos;
-			});
+            if (isDrag) {
+                delta = dist(_mPos, _mStartPos).delta;
+                status.append(
+                    '<label>X<sub>1</sub> ' + _mStartPos.x
+                    + ' Y<sub>1</sub> ' + _mStartPos.y + '</label>'
+                    + '<label>&Delta; ' + delta + '</label>'
+                );
+            }
 
-			_canvas.on('mousedown', function(e) {
-				if (!isDrag) {
-					isDrag = true;
-					_mStartPos.x = e.pageX - _cPos.x;
-					_mStartPos.y = e.pageY - _cPos.y;
-				}
-			});
+            // record the position for next frame
+            _mOldPos = _mPos;
+        });
 
-			_canvas.on('mouseup', function(e) {
-				if (isDrag) {
-					isDrag = false;
-				}
-			});
+        _canvas.on('mousedown', function (e) {
+            if (!isDrag) {
+                isDrag = true;
+                _mStartPos.x = e.pageX - _cPos.x;
+                _mStartPos.y = e.pageY - _cPos.y;
+            }
+        });
 
-			// modify canvas size
-			var setSizePanel = $('#set_cSize'),
-			toolBox = $('#toolBox');
+        _canvas.on('mouseup', function (e) {
+            if (isDrag) {
+                isDrag = false;
+            }
+        });
 
-			$('#set_cWidth').val(_cWidth);
-			$('#set_cHeight').val(_cHeight);
-			setSizePanel.on('change', 'input', function() {
-				var that = $(this), val = that.val();
-				if (val <= 100 || val > 1200) {
-					val = val <= 100 ? 100 : 1200;
-					that.val(val);
-				}
+        // modify canvas size
+        var setSizePanel = $('#set_cSize'),
+            toolBox = $('#toolBox');
 
-				if (that.attr('id') == 'set_cWidth') {
-					_cWidth = +val;
-					_canvas.attr('width', _cWidth);
+        $('#set_cWidth').val(_cWidth);
+        $('#set_cHeight').val(_cHeight);
+        setSizePanel.on('change', 'input', function () {
+            var that = $(this), val = that.val();
+            if (val <= 100 || val > 1200) {
+                val = val <= 100 ? 100 : 1200;
+                that.val(val);
+            }
 
-					status.css('width', _cWidth);
-					setSizePanel.css('width', _cWidth >= 300 ? _cWidth : 300);
-					toolBox.css('left', _cWidth >= 300 ? _cWidth + 35 : 335);
-				}
-				else if (that.attr('id') == 'set_cHeight') {
-					_cHeight = +val;
-					_canvas.attr('height', _cHeight);
-					toolBox.css('height', _cHeight + 70);
-				}
+            if (that.attr('id') == 'set_cWidth') {
+                _cWidth = +val;
+                _canvas.attr('width', _cWidth);
 
-				// update rulers
-				setupRulers();
+                status.css('width', _cWidth);
+                setSizePanel.css('width', _cWidth >= 300 ? _cWidth : 300);
+                toolBox.css('left', _cWidth >= 300 ? _cWidth + 35 : 335);
+            }
+            else if (that.attr('id') == 'set_cHeight') {
+                _cHeight = +val;
+                _canvas.attr('height', _cHeight);
+                toolBox.css('height', _cHeight + 70);
+            }
 
-				// reset the markers
-				hMarker.css('left', 0);
-				vMarker.css('top', 0);
-			}).css('width', _cWidth);
+            // update rulers
+            setupRulers();
 
-			// toggle grid
-			$('#gridOn').on('click', function(e) {
-				if ($(this).prop('checked')) {
-					_canvas.addClass('grid');
-				}
-				else {
-					_canvas.removeClass('grid');
-				}
-			});
+            // reset the markers
+            hMarker.css('left', 0);
+            vMarker.css('top', 0);
+        }).css('width', _cWidth);
 
-		});
+        // toggle grid
+        $('#gridOn').on('click', function (e) {
+            if ($(this).prop('checked')) {
+                _canvas.addClass('grid');
+            }
+            else {
+                _canvas.removeClass('grid');
+            }
+        });
 
-		function dist(p1, p2) {
-			var dx = p2.x - p1.x,
-			dy = p2.y - p1.y;
+    });
 
-			return { delta: Math.sqrt( dx*dx + dy*dy ) | 0, x: dx, y: dy };
-		}
-	},false);
+    function dist(p1, p2) {
+        var dx = p2.x - p1.x,
+            dy = p2.y - p1.y;
+
+        return {delta: Math.sqrt(dx * dx + dy * dy) | 0, x: dx, y: dy};
+    }
+}, false);
